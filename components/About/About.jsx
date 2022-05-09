7183ab6b3f80595c0d8aa3e1f26e9250adc755d0
Misch@ <Michaela.pupikova22@gmail.com>
2022-05-09 09:12:32 +0200

    commit message

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
       <h2 className={styles.sectionTitle}>What I do</h2>
       <div className={styles.services}>
         <AboutCard
-          cardHeader={"Design + Development"}
+          cardHeader={'Design + Development'}
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
         incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
         exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
--- components/Projects/Projects.jsx
+++ components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectItem from '../ProjectItem/ProjectItem';
 
 const Projects = () => {
   return (
-    <section className={styles.myWork} id="work">
+    <section className={styles.projects} id="work">
       <h2 className={styles.sectionTitle}>My work</h2>
       <p className={styles.sectionSubtitle}>A selection of my range of work</p>
       <div className={styles.portfolio}>
